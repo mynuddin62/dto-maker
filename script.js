a8214696a3f6daecc96817340fb64df5fb7ea2f3
mynuddin62 <mynuddinratul@gmail.com>
2024-10-28 08:52:12 +0600

    adding types

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function convertToTypeScript() {
     const javaDTO = document.getElementById("javaDTO").value;
+    if (!javaDTO.trim()) {
+        document.getElementById("tsDTO").value = "Please enter Java DTO fields.";
+        return;
+    }
+
     const lines = javaDTO.split("\n");
     let tsDTO = "";
 
@@ -22,20 +27,32 @@ function convertToTypeScript() {
                     break;
                 case "Integer":
                 case "int":
+                case "double":
+                case "Double":
+                case "Long":
                     tsType = "number";
                     break;
                 case "Boolean":
                     tsType = "boolean";
+                    break;
+                case "Date":
+                    tsType = "Date";
                     break;
                 default:
                     tsType = "any";
             }
 
             // Convert field name to camelCase and append '?'
+            fieldName = toCamelCase(fieldName);
             tsDTO += `  ${fieldName}?: ${tsType};\n`;
         }
     });
 
     // Display the TypeScript DTO
     document.getElementById("tsDTO").value = tsDTO;
+}
+
+// Helper function to convert field name to camelCase
+function toCamelCase(fieldName) {
+    return fieldName.replace(/_([a-z])/g, (g) => g[1].toUpperCase());
 }
